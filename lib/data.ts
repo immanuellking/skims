3718cfd1f8c482af55147b4f44d213235cb0f942
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-26 10:23:55 +0100

    data for sub header nd nav links and dropdown data

---
+++ lib/data.ts
@@ -0,0 +1,238 @@
+
+export const texts = [
+    "International Shipping Available",
+    "Curated Pickup For you",
+    "Return Policy",
+  ];
+
+export const dropdown = [
+  {
+    title: "NEW",
+
+    linksContent: [
+      {
+        tab: "ALL NEW ARRIVALS",
+        links: ["BACK IN STOCK"],
+      },
+      {
+        tab: "DROPS",
+        links: [
+          "DROPS",
+          "New Raw Edge",
+          "New Essential Bodysuits",
+          "New Stretch Lace",
+          "New Fit Everybody",
+          "New Skims Jacquard",
+        ],
+      },
+    ],
+
+    imageContent: [
+      {
+        src: "/header/new-1.webp",
+        header: "THE WINTER SHOP",
+      },
+      {
+        src: "/header/new-2.webp",
+        header: "THE T-SHIRT SHOP",
+      },
+      {
+        src: "/header/new-3.webp",
+        header: "SKIMS MENS",
+      },
+    ],
+  },
+  {
+    title: "BEST SELLERS",
+  },
+  {
+    title: "BRAS",
+    linksContent: [
+      {
+        tab: "ALL BRAS",
+        links: ["ALL BRALETTES", "FIND YOUR SIZE", "THE BRA GUIDE", "LINGERIE"],
+      },
+      {
+        tab: "SILHOUETE",
+        links: [
+          "T-Shirt",
+          "Scoop",
+          "Demi",
+          "Triangle",
+          "Plunge",
+          "Strapless",
+          "Blconette",
+        ],
+      },
+      {
+        tab: "LINING",
+        links: ["Unlined", "Lightly Lined", "Push-Up"],
+      },
+    ],
+
+    imageContent: [
+      {
+        src: "/header/bra-1.webp",
+        header: "THE WINTER SHOP",
+        subText:
+          "The Perfect Everybody Bra With Next-Level Stretch And Comfort",
+      },
+      {
+        src: "/header/bra-2.webp",
+        header: "THE T-SHIRT SHOP",
+        subText: "This Is One Bralette You'll Never Want To Take Off",
+      },
+    ],
+  },
+  {
+    title: "UNDERWEAR",
+    linksContent: [
+      {
+        tab: "ALL UNDERWEAR",
+        links: ["LINGERIE", "BUNDLES & SAVE"],
+      },
+      {
+        tab: "STYLE",
+        links: [
+          "Thongs",
+          "Boxers",
+          "Boy Shorts",
+          "Cheeky Underwear",
+          "Briefs",
+          "Hoisery",
+        ],
+      },
+      {
+        tab: "COVERAGE",
+        links: ["Minimum", "Medium", "Maximum"],
+      },
+    ],
+
+    imageContent: [
+      {
+        src: "/header/underwear.webp",
+        header: "BUNDLE & SAVE",
+        subText: "Mix & Match Best-Selling Styles At Special Pricing",
+      },
+    ],
+  },
+  {
+    title: "CLOTHING & LOUNGE",
+    linksContent: [
+      {
+        tab: "ALL LOUNGEWEAR",
+        links: ["ALL CLOTHING", "FABRIC GUIDE"],
+      },
+      {
+        tab: "CLOTHING & LOUNGE",
+        links: [
+          "Tees & Tanks",
+          "Leggings & Pants",
+          "Dresses",
+          "Bodysuits",
+          "Pajamas",
+          "Hoodies & Sweaters",
+          "Catsuits & Onesies",
+        ],
+      },
+    ],
+    imageContent: [
+      {
+        src: "/header/lounge-1.webp",
+        header: "T-SHIRTS",
+        subText: "Your One-Stop For Tees And Tanks",
+      },
+      {
+        src: "/header/lounge-2.webp",
+        header: "LEGGINGS & PANTS",
+        subText: "Versatile Styles And Silhouetes For Weather",
+      },
+      {
+        src: "/header/lounge-3.webp",
+        header: "PAJAMAS",
+        subText: "From Sleep Sets To Robe",
+      },
+    ],
+  },
+  {
+    title: "MENS",
+    linksContent: [
+      {
+        tab: "EXPLORE MENS",
+        links: ["ALL MENS", "BUNDLES & SAVE"],
+      },
+      {
+        tab: "SHOP BY",
+        links: [
+          "Loungewear",
+          "Hoodies & Sweatshirts",
+          "Bottoms",
+          "Boxers & Briefs",
+          "Tees & Tanks",
+          "Socks",
+        ],
+      },
+    ],
+    imageContent: [
+      {
+        src: "/header/mens-1.webp",
+        header: "FLEECE LOUNGE",
+        subText:
+          "Classic Styles Made Of Stretchy Cotton With A Soft Feel And Loose Fit",
+      },
+      {
+        src: "/header/mens-2.webp",
+        header: "JERSEY LOUNGE",
+        subText:
+          "Stretchy Weighty Cotton Styles For Ultimate Comfort While Lounging And Beyond",
+      },
+    ],
+  },
+  {
+    title: "SALES",
+  },
+  {
+    title: "COLLECTION",
+    linksContent: [
+      {
+        tab: "FEATURED",
+        links: [
+          "Fits Everybody",
+          "Soft Lounge",
+          "Seamless Sculpt",
+          "Cotton Collection",
+          "Cotton Fleece",
+          "Shop All Collection",
+        ],
+      },
+      {
+        tab: "SHOP",
+        links: [
+          "The T-Shirt Shop",
+          "The Sleep Edit",
+          "Kim's Picks",
+          "The Winter Shop",
+          "The Dress Shop",
+        ],
+      },
+    ],
+    imageContent: [
+      {
+        src: "/header/collection-1.webp",
+        header: "SOFT LOUNGE",
+        subText:
+          "Ultra-Soft Ribbed Loungewear And Sleepwear, Designed For At Home And Beyond",
+      },
+      {
+        src: "/header/collection-2.webp",
+        header: "COTTON",
+        subText: "The Selfie-Worthy Sets You've Seen All Over Your Feed",
+      },
+      {
+        src: "/header/collection-3.webp",
+        header: "FITS EVERYBODY",
+        subText: "Our Best-Selling Buttery-Soft Styles With Next-Level Stretch",
+      },
+    ],
+  },
+];
