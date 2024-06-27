21696bbaa530216bd9cf798859c779bbde032da9
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-27 19:40:24 +0100

    added schema to sanity schem types

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import justIn from "./schemas/just-in";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [justIn],
+};
