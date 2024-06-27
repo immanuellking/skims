a6f8aae2ad79176c9719f8f3c3dfa0df4ba0715c
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-27 19:39:39 +0100

    defined schemas for justin items

---
+++ sanity/schemas/just-in.ts
@@ -0,0 +1,74 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "justIn",
+  title: "Just In",
+  type: "document",
+  fields: [
+    defineField({
+      name: "tag",
+      title: "Tag",
+      type: "string",
+    }),
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "string",
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+    }),
+    defineField({
+      name: "colors",
+      title: "Colors",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
+    }),
+    defineField({
+      name: "status",
+      title: "Status",
+      type: "string",
+      options: {
+        list: [
+          { title: "Hot Sale", value: "hot_sale" },
+          { title: "New", value: "new" },
+          { title: "Nil", value: "nil" },
+        ],
+      },
+    }),
+    defineField({
+      name: "size",
+      title: "Size",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
+    }),
+    defineField({
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [
+        {
+          type: "image",
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+    }),
+  ],
+});
