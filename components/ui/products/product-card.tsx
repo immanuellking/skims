71bb4c98d019eb53f15b2c2944e717d1ad5d81a4
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-30 00:03:36 +0100

    product card with images name, image and price fo the section

---
+++ components/ui/products/product-card.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+import { urlForImage } from "@/sanity/lib/image";
+import { JustIn } from "@/typing";
+import Image from "next/image";
+
+export default function ProductCard({ product }: { product: JustIn }) {
+  return (
+    <div className="relative w-[300px] space-y-2 group cursor-pointer">
+      <div className="relative w-[300px] h-[300px] overflow-hidden">
+        <Image
+          src={urlForImage(product.images[0])}
+          alt={product.name}
+          // width={280}
+          // height={280}
+          fill
+          className="object-contain group-hover:scale-105 transition-all duration-700 ease-in-out"
+        />
+      </div>
+      {product.status !== "nil" && (
+        <span
+          className={cn(
+            "absolute top-0 right-0 bg-green-500 text-xs w-20 py-[5px] capitalize text-white text-center rounded-sm",
+            {
+              "bg-red-500": product.status === "hot sale",
+            }
+          )}
+        >
+          {product.status}
+        </span>
+      )}
+      <div className="w-full text-center text-[13px] uppercase font-semibold space-y-1 text-[#62554A]">
+        <p>
+          {product.tag} {product.name}
+        </p>
+        <p>&#8358; {product.price.toLocaleString()}</p>
+      </div>
+    </div>
+  );
+}
