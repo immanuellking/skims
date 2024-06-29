3abff9f2ba9ed06572ae11438d2c152991089ce5
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-30 00:13:51 +0100

    added justIn section to home page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import JustIN from "@/components/just-in";
 
 export default function Home() {
   return (
     <main className="w-full">
       <Hero />
+      <JustIN />
     </main>
   );
 }
