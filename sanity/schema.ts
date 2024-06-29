24fcfce47345107c149b6d543c2f1707dad0d775
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-29 08:21:17 +0100

    added trending products scheme to schema types

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 import justIn from "./schemas/just-in";
+import trending from "./schemas/trending";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [justIn],
+  types: [justIn, trending],
 };
