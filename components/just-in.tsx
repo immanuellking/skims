b557736dfe0084a5d04eb183ca30b831973b6e5c
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-30 01:31:02 +0100

    added loading skeleton to products

--- components/just-in.tsx
+++ components/just-in.tsx
@@ -1,15 +1,14 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import Products from "./ui/products/products";
+import { ProductsCardsSkeleton } from "./skeletons";
 
 export default async function JustIN() {
-  
-
   return (
     <div className="w-full">
       <div className="w-full text-center text-[#62554A] text-3xl font-semibold py-16">
         <h1>Just In</h1>
       </div>
-      <Suspense fallback={<h1>Loading............</h1>}>
+      <Suspense fallback={<ProductsCardsSkeleton />}>
         <Products type="justIn" />
       </Suspense>
       <div className="w-full flex justify-center mt-10">
