d0f8dfb9ab33c62479a10881885e46d267adfcd3
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-02 02:50:13 +0100

    products carousel responsive on mobile

--- components/ui/products/products.tsx
+++ components/ui/products/products.tsx
@@ -11,20 +11,20 @@ import { fetchProducts } from "@/lib/queries";
 export default async function Products({ type }: { type: string }) {
   const products = await fetchProducts(type);
   return (
-    <div className="w-[90%] mx-auto">
-      <Carousel opts={{ align: "start" }}>
+    // <div className="relative w-full lg:w-[90%] lg:mx-auto p-0 m-0">
+      <Carousel opts={{ align: "start" }} className="w-[95%] sm:w-[90%] mx-auto">
         <CarouselContent>
           {products.map((product) => {
             return (
-              <CarouselItem className="basis-1/4" key={product.id}>
+              <CarouselItem className="basis-1/2 sm:basis-1/3 lg:basis-1/4" key={product.id}>
                 <ProductCard product={product} />
               </CarouselItem>
             );
           })}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <CarouselPrevious className="hidden sm:flex" />
+        <CarouselNext className="hidden sm:flex" />
       </Carousel>
-    </div>
+    // </div>
   );
 }
