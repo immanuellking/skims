30c19902d2e03a2c9b5e8a7ca484088c23be852f
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-02 02:48:49 +0100

    made section responsive and component also reusable fo fething different section data

--- components/just-in.tsx
+++ components/just-in.tsx
@@ -1,18 +1,32 @@
 import { Suspense } from "react";
 import Products from "./ui/products/products";
 import { ProductsCardsSkeleton } from "./skeletons";
+import { cn } from "@/lib/utils";
 
-export default async function JustIN() {
+export default async function JustIN({
+  title,
+  type,
+  disable,
+}: {
+  title: string;
+  type: string;
+  disable?: boolean;
+}) {
   return (
-    <div className="w-full">
-      <div className="w-full text-center text-[#62554A] text-3xl font-semibold py-16">
-        <h1>Just In</h1>
+    <div className="w-full py-8 xs:py-10 sm:py-16">
+      <div className="w-full text-center text-[#62554A] text-3xl font-semibold mb-10 xs:mb-14">
+        <h1>{title}</h1>
       </div>
       <Suspense fallback={<ProductsCardsSkeleton />}>
-        <Products type="justIn" />
+        <Products type={type} />
       </Suspense>
-      <div className="w-full flex justify-center mt-10">
-        <button className="py-2 uppercase w-72 bg-[#62554A] text-white font-semibold rounded-sm">
+      <div className="w-full flex justify-center mt-5 sm:mt-10">
+        <button
+          className={cn(
+            "py-2 xs:py-3 uppercase w-60 xs:w-72 bg-[#62554A] hover:bg-[#8b7a6c] transition-all ease-in duration-300 text-white text-sm xs:text-base font-medium xs:font-semibold rounded-sm",
+            { hidden: disable }
+          )}
+        >
           Shop All New Arrivals
         </button>
       </div>
