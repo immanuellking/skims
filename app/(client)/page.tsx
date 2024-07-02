a34732af9f6a224ad4a7bbd1cbb6692f94d4e9d2
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-02 15:55:12 +0100

    added shop now section to home page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,6 +1,7 @@
 import Categories from "@/components/categories";
 import Hero from "@/components/hero";
 import JustIN from "@/components/just-in";
+import ShopNow from "@/components/shop-now";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <JustIN title="Just In" type="justIn" />
       <Categories />
       <JustIN title="Trending" type="trending" disable={true} />
+      <ShopNow />
     </main>
   );
 }
