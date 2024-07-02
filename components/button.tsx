286852a42d228a4ea778d0640d685a63cc3f4306
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-02 15:54:36 +0100

    made hero button into a component to be reusable

---
+++ components/button.tsx
@@ -0,0 +1,8 @@
+
+export default function Button() {
+  return (
+    <button className="w-56 xs:w-64 bg-[#EEE4DA] py-2 xs:py-3 rounded-md font-semibold transition-all duration-200 ease-in-out hover:text-white text-sm xs:text-base hover:bg-[#d9bb9e]">
+      Shop Now
+    </button>
+  );
+}
