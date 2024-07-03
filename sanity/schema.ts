4ee34e607f91a45493d525e002af27258774da9d
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-03 10:00:53 +0100

    add fits schema to schema types

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import justIn from "./schemas/just-in";
 import trending from "./schemas/trending";
+import fits from "./schemas/fits";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [justIn, trending],
+  types: [justIn, trending, fits],
 };
