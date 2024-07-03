340b22465a0e4fe54e121dfae9e53fdb9b9b73fb
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-03 10:04:13 +0100

    refactor carousel and carousel item to receive their classes from props

--- components/ui/products/products.tsx
+++ components/ui/products/products.tsx
@@ -8,23 +8,31 @@ import {
 import ProductCard from "./product-card";
 import { fetchProducts } from "@/lib/queries";
 
-export default async function Products({ type }: { type: string }) {
+export default async function Products({
+  type,
+  parentClass,
+  cardClass,
+}: {
+  type: string;
+  parentClass: string;
+  cardClass: string;
+}) {
+
   const products = await fetchProducts(type);
+  
   return (
-    // <div className="relative w-full lg:w-[90%] lg:mx-auto p-0 m-0">
-      <Carousel opts={{ align: "start" }} className="w-[95%] sm:w-[90%] mx-auto">
-        <CarouselContent>
-          {products.map((product) => {
-            return (
-              <CarouselItem className="basis-1/2 sm:basis-1/3 lg:basis-1/4" key={product.id}>
-                <ProductCard product={product} />
-              </CarouselItem>
-            );
-          })}
-        </CarouselContent>
-        <CarouselPrevious className="hidden sm:flex" />
-        <CarouselNext className="hidden sm:flex" />
-      </Carousel>
-    // </div>
+    <Carousel opts={{ align: "start" }} className={parentClass}>
+      <CarouselContent>
+        {products.map((product) => {
+          return (
+            <CarouselItem className={cardClass} key={product.id}>
+              <ProductCard product={product} />
+            </CarouselItem>
+          );
+        })}
+      </CarouselContent>
+      <CarouselPrevious className="hidden sm:flex" />
+      <CarouselNext className="hidden sm:flex" />
+    </Carousel>
   );
 }
