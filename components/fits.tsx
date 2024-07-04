b0af333b541a79cc8881c9a2aac14f0506e008a9
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-04 19:28:13 +0100

    made adjustments to fits section stykes

--- components/fits.tsx
+++ components/fits.tsx
@@ -5,8 +5,8 @@ import { FitsProductsCardsSkeleton } from "./skeletons";
 
 export default async function Fits() {
   return (
-    <section className="w-full flex flex-col lg:flex-row xs:py-10 lg:py-16 gap-8">
-      <div className="relative h-[45vh] lg:h-[80vh] w-[100%] lg:w-[55%]">
+    <section className="w-full flex flex-col lg:flex-row py-8 sm:py-10 lg:py-16 gap-8">
+      <div className="relative h-[50vh] xs:h-[45vh] lg:h-[80vh] w-[100%] lg:w-[55%]">
         <Image
           src="/categories/fits.webp"
           alt="best fits"
@@ -15,13 +15,13 @@ export default async function Fits() {
         />
 
         <div className="block lg:hidden space-y-2 absolute top-[50%] left-[1rem] text-white">
-          <h1 className="text-3xl uppercase font-semibold">
+          <h1 className="text-2xl xs:text-3xl uppercase font-semibold">
             3 fits for $300
           </h1>
           <p className="text-sm">
             Shop our best-selling styles at special pricing.
           </p>
-          <span className="underline mt-2">shop now</span>
+          <span className="underline mt-4 capitalize font-medium">shop all</span>
         </div>
       </div>
       <div className="w-[100%] lg:w-[45%] h-full space-y-8">
@@ -36,8 +36,8 @@ export default async function Fits() {
         <Suspense fallback={<FitsProductsCardsSkeleton />}>
           <Products
             type="fits"
-            parentClass="w-[85%] lg:w-[90%] mx-auto"
-            cardClass="basis-1/2 lg:basis-1/2"
+            parentClass="w-[95%] xs:w-[85%] lg:w-[90%] mx-auto"
+            cardClass="basis-[50%]"
           />
         </Suspense>
       </div>
