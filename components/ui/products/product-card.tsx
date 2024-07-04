59315d8cd00924ae67c9190aa35cfcdfdb98dd18
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-04 19:24:56 +0100

    adjested styles for product card

--- components/ui/products/product-card.tsx
+++ components/ui/products/product-card.tsx
@@ -5,15 +5,15 @@ import Image from "next/image";
 
 export default function ProductCard({ product }: { product: JustIn }) {
   return (
-    <div className="relative w-full space-y-2 group cursor-pointer">
-      <div className="relative w-[200px] sm:w-[300px] h-[300px] overflow-hidden">
+    <div className="relative w-full sm:w-[300px] space-y-2 cursor-pointer">
+      <div className="relative w-full h-[220px] sm:w-[300px] sm:h-[300px]">
         <Image
           src={urlForImage(product.images[0])}
           alt={product.name}
           // width={280}
           // height={280}
           fill
-          className="object-contain group-hover:scale-105 transition-all duration-700 ease-in-out"
+          className="object-contain object-center"
         />
       </div>
       {product.status !== "nil" && (
@@ -28,7 +28,7 @@ export default function ProductCard({ product }: { product: JustIn }) {
           {product.status}
         </span>
       )}
-      <div className="w-full text-center text-[13px] uppercase font-semibold space-y-1 text-[#62554A]">
+      <div className="w-full text-center text-xs sm:text-[13px] uppercase font-semibold space-y-1 text-[#62554A]">
         <p>
           {product.tag}
           <br className="lg:hidden" />
