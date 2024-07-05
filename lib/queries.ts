f350ab6d5d35c82f512e3a3eb93b1d6519d555aa
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-05 13:56:15 +0100

    implemented and exported function for getting review posts from sanity using groq

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,5 +1,5 @@
 import { client } from "@/sanity/lib/client";
-import { JustIn } from "@/typing";
+import { JustIn, ReviewPosts } from "@/typing";
 import { groq } from "next-sanity";
 import { unstable_noStore as noStore } from "next/cache";
 
@@ -14,6 +14,20 @@ export const fetchProducts = async (type: string) => {
     return justInProducts;
   } catch (error) {
     console.log("DB Error", error);
-    throw new Error("Failed to fetch Just in Products");
+    throw new Error("Failed to fetch Products");
+  }
+};
+
+export const fetchReviews = async () => {
+  noStore();
+
+  const query = groq`*[_type == "reviews"]`;
+
+  try {
+    const posts: ReviewPosts[] = await client.fetch(query);
+    return posts;
+  } catch (error) {
+    console.log("DB Error", error);
+    throw new Error("Failed to fetch Reviews");
   }
 };
