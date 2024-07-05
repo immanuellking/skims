326f51831ed11182a92e623b593602b7de07e6b1
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-05 11:15:32 +0100

    added review defined scheme to sanity schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 import justIn from "./schemas/just-in";
 import trending from "./schemas/trending";
 import fits from "./schemas/fits";
+import reviews from "./schemas/reviews";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [justIn, trending, fits],
+  types: [justIn, trending, fits, reviews],
 };
