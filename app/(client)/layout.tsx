2339e7a6aa231affee07c81e32ce51326dddfd39
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-09 16:10:58 +0100

    added footer to root layout

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { poppins } from "@/components/ui/font";
 import "./globals.css";
 import Header from "@/components/ui/header";
+import Footer from "@/components/ui/footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,6 +21,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
