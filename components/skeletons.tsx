c709939ad2e4de7994cb53589ef4474f333936bc
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-12 09:37:32 +0100

    built loading skeleton for product detials page and exported

--- components/skeletons.tsx
+++ components/skeletons.tsx
@@ -71,3 +71,64 @@ export const ReviewPostsCardsSkeleton = () => {
     </div>
   );
 };
+
+export const ProductDetailsPageSkeleton = () => {
+  return (
+    <main
+      className={`${shimmer} w-[90%] mx-auto pt-8 xs:pt-10 lg:pt-16 pb-16 xs:pb-20 lg:pb-24`}
+    >
+      <div className="flex flex-col lg:flex-row gap-8">
+        <div className="flex flex-col-reverse sm:flex-row gap-8 w-full lg:w-[50%]">
+          <div className="flex flex-row sm:flex-col gap-2 sm:gap-4">
+            <div className={`w-[7rem] h-[6rem] sm:h-[8rem] bg-[#f2ded4]`}></div>
+            <div className={`w-[7rem] h-[6rem] sm:h-[8rem] bg-[#f2ded4]`}></div>
+            <div className={`w-[7rem] h-[6rem] sm:h-[8rem] bg-[#f2ded4]`}></div>
+            <div className={`w-[7rem] h-[6rem] sm:h-[8rem] bg-[#f2ded4]`}></div>
+          </div>
+          <div className="w-full h-[25rem] sm:h-[38rem] lg:h-[35rem] bg-[#f2ded4]"></div>
+        </div>
+
+        <div className="w-full lg:w-[50%]">
+          <div className="space-y-4 lg:space-y-2">
+            <h4 className="h-6 w-24 bg-[#f2ded4]"></h4>
+            <div className="h-10 w-[60%] lg:w-full bg-[#f2ded4]"></div>
+            <div className="h-6 w-[40%] bg-[#f2ded4]"></div>
+          </div>
+
+          <div className="mt-5 ">
+            <div className="space-y-2">
+              <h1 className="h-6 w-[10%] bg-[#f2ded4]"></h1>
+              <div className="flex gap-x-2">
+                <div className="h-7 w-7 rounded-full bg-[#f2ded4]"></div>
+                <div className="h-7 w-7 rounded-full bg-[#f2ded4]"></div>
+                <div className="h-7 w-7 rounded-full bg-[#f2ded4]"></div>
+              </div>
+            </div>
+            <div className="space-y-2 mt-4">
+              <div className="flex justify-between">
+                <h1 className="h-6 w-[10%] bg-[#f2ded4]"></h1>
+                <h1 className="h-6 w-[10%] bg-[#f2ded4]"></h1>
+              </div>
+              <div className="flex gap-x-2">
+                <div className="h-10 w-10 bg-[#f2ded4] rounded-full"></div>
+                <div className="h-10 w-10 bg-[#f2ded4] rounded-full"></div>
+                <div className="h-10 w-10 bg-[#f2ded4] rounded-full"></div>
+              </div>
+            </div>
+
+            <button className="bg-[#EEE4DA] mt-8 h-12 w-full rounded-3xl"></button>
+          </div>
+
+          <div className="mt-5 h-40 sm:h-48 w-full bg-[#f2ded4]"></div>
+        </div>
+      </div>
+
+      <div className="mt-16 xs:mt-20 lg:mt-28">
+        <div className="w-full mb-10 xs:mb-14 flex justify-center">
+          <h1 className="h-12 w-[60%] lg:w-[50%] bg-[#EEE4DA]"></h1>
+        </div>
+        <ProductsCardsSkeleton />
+      </div>
+    </main>
+  );
+};
