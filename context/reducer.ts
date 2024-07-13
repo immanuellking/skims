f90b0e54a0daad005d7b7b1d9cbfc8cb372d80ea
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-13 15:07:44 +0100

    switch case for adding item to cart done

--- context/reducer.ts
+++ context/reducer.ts
@@ -3,7 +3,13 @@ import { ActionType, StateType } from "@/typing";
 export const reducer = (state: StateType, action: ActionType) => {
   switch (action.type) {
     case "ADD_ITEM_TO_CART":
-      console.log("cart adding working");
+      const itemAlreadyExist = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemAlreadyExist) {
+        return state;
+      }
 
       return { ...state, cart: [action.payload, ...state.cart] };
   }
