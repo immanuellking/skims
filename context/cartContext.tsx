2bf1d03a041d8115631174f187cbb3d8ea93eb2f
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-13 15:09:20 +0100

    fixed server error by checking if the enviroment is client or server before accessing the local storage on the clien env

--- context/cartContext.tsx
+++ context/cartContext.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { ActionType, CartType, StateType } from "@/typing";
-import { ReactNode, createContext, useContext, useReducer } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
 import { reducer } from "./reducer";
 
 const initialState: StateType = {
@@ -15,11 +21,26 @@ const CartContext = createContext<{
 }>({ state: initialState, dispatch: () => null, addItemToCart: () => {} });
 
 function CartProvider({ children }: { children: ReactNode }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState, (initial) => {
+    // Check if window is defined (browser environment)
+    if (typeof window !== "undefined") {
+      const localData = localStorage.getItem("cart");
+      return localData ? { cart: JSON.parse(localData) } : initial;
+    }
+    return initial;
+  });
 
   function addItemToCart(item: CartType) {
     dispatch({ type: "ADD_ITEM_TO_CART", payload: item });
   }
+
+  useEffect(() => {
+    // Save state to localStorage
+    if (typeof window !== "undefined") {
+      // Save state to localStorage
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    }
+  }, [state.cart]);
 
   return (
     <CartContext.Provider value={{ state, dispatch, addItemToCart }}>
