8ecdbe48d2156451352ff7cdc32aca67f240cd65
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-16 09:46:28 +0100

    add total to initial state and also to the context argument

--- context/cartContext.tsx
+++ context/cartContext.tsx
@@ -12,26 +12,32 @@ import { reducer } from "./reducer";
 
 const initialState: StateType = {
   cart: [],
+  total: 0,
 };
 
 const CartContext = createContext<{
   state: StateType;
   dispatch: React.Dispatch<ActionType>;
   addItemToCart: (item: CartType) => void;
-}>({ state: initialState, dispatch: () => null, addItemToCart: () => {} });
+  getTotal: () => void
+}>({ state: initialState, dispatch: () => null, addItemToCart: () => {}, getTotal: () => {} });
 
 function CartProvider({ children }: { children: ReactNode }) {
   const [state, dispatch] = useReducer(reducer, initialState, (initial) => {
     // Check if window is defined (browser environment)
     if (typeof window !== "undefined") {
       const localData = localStorage.getItem("cart");
-      return localData ? { cart: JSON.parse(localData) } : initial;
+      return localData ? { cart: JSON.parse(localData), total: 0 } : initial;
     }
     return initial;
   });
 
   function addItemToCart(item: CartType) {
     dispatch({ type: "ADD_ITEM_TO_CART", payload: item });
+  }
+
+  function getTotal() {
+    dispatch({ type: "GET_TOTAL" });
   }
 
   useEffect(() => {
@@ -43,7 +49,7 @@ function CartProvider({ children }: { children: ReactNode }) {
   }, [state.cart]);
 
   return (
-    <CartContext.Provider value={{ state, dispatch, addItemToCart }}>
+    <CartContext.Provider value={{ state, dispatch, addItemToCart, getTotal }}>
       {children}
     </CartContext.Provider>
   );
