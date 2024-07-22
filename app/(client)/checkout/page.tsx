daf2d619510ecba89a5a0656399b6ac8d3483226
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-22 17:32:07 +0100

    checkout page and route, added the checkout component and the page title

---
+++ app/(client)/checkout/page.tsx
@@ -0,0 +1,14 @@
+import Checkout from "@/components/ui/checkout/checkout";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="w-[95%] lg:w-[90%] mx-auto py-10 xs:py-16 space-y-8">
+      <h1 className="text-[30px] sm:text-4xl lg:text-5xl font-medium text-[#62554a]">
+        Checkout
+      </h1>
+
+      <Checkout />
+    </div>
+  );
+}
