6aa7d16d92ab3c98e57b89ac082a207343242d59
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-23 12:03:57 +0100

    built address ui and implemented deleting address

--- components/ui/checkout/address.tsx
+++ components/ui/checkout/address.tsx
@@ -1,20 +1,19 @@
 "use client";
-
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { AddressForm } from "./address-form";
 import { useCart } from "@/context/cartContext";
+import { HiOutlinePencilSquare, HiOutlineTrash } from "react-icons/hi2";
 
 export function Address() {
-  const { openDialog, state } = useCart();
+  const { openDialog, state, deleteAddress } = useCart();
   return (
-    <section className="w-[50%]">
+    <section className="w-full lg:w-[50%] space-y-2 lg:space-y-4">
       <div className="flex justify-between border-b border-gray-400 py-2 ">
         <h4 className="text-[#62554a] font-medium">Choose Address</h4>
         <p
@@ -26,7 +25,32 @@ export function Address() {
         <AddressDialog />
       </div>
 
-      
+      {state.addresses.length >= 1 ? (
+        <div className="space-y-4">
+          {state.addresses.map((add) => (
+            <div className="shadow-sm p-4" key={add.id}>
+              <h3 className="text-xl font-semibold capitalize text-[#62554a]">
+                {add.name}
+              </h3>
+              <p className="text-sm text-[#62554a]">
+                {add.houseNo}, {add.street}, {add.city}, {add.state},{" "}
+                {add.postalCode}
+              </p>
+              <p className="text-sm text-[#62554a]">{add.phoneNumber}</p>
+              <div className="mt-2 space-x-4">
+                <button>
+                  <HiOutlinePencilSquare className="text-lg text-green-400" />
+                </button>
+                <button onClick={() => deleteAddress(add.id)}>
+                  <HiOutlineTrash className="text-xl text-red-500" />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (<div>
+        <p>You have no addresses yet...</p>
+      </div>)}
     </section>
   );
 }
