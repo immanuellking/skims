58b392da52aa839d2b4caaaa69587924ec6b24b9
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-26 22:06:01 +0100

    added payment section for step 3 of checkout

--- components/ui/checkout/address.tsx
+++ components/ui/checkout/address.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { useCart } from "@/context/cartContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChooseAddress from "./choose-address";
 import SelectedAddress from "./selected-address";
 import { AddressDialog } from "./address-dialog";
+import Payments from "./payments";
 
 export function Address() {
   const [step, setStep] = useState(1);
@@ -17,25 +18,31 @@ export function Address() {
     openDialog();
   }
 
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [step]);
+
   return (
     <section className="w-full lg:w-[50%] space-y-2 lg:space-y-4">
-      <div className="flex justify-between items-end border-b border-gray-400 py-2 ">
-        <h4 className="text-[#62554a] font-medium">
-          {step === 1 ? "Choose Address" : "Confirm Address "}
-        </h4>
-        {step === 1 ? (
-          <p className="text-green-500 cursor-pointer" onClick={addAddress}>
-            Add Address
-          </p>
-        ) : (
-          <p
-            className="text-[#62554a] font-medium cursor-pointer text-sm"
-            onClick={() => setStep((prev) => prev - 1)}
-          >
-            go back
-          </p>
-        )}
-      </div>
+      {step < 3 && (
+        <div className="flex justify-between items-end border-b border-gray-400 py-2 ">
+          <h4 className="text-[#62554a] font-medium">
+            {step === 1 ? "Choose Address" : "Confirm Address "}
+          </h4>
+          {step === 1 ? (
+            <p className="text-green-500 cursor-pointer" onClick={addAddress}>
+              Add Address
+            </p>
+          ) : (
+            <p
+              className="text-[#62554a] font-medium cursor-pointer text-sm"
+              onClick={() => setStep((prev) => prev - 1)}
+            >
+              go back
+            </p>
+          )}
+        </div>
+      )}
 
       {state.addresses.length >= 1 ? (
         <div className="space-y-6 sm:space-y-8">
@@ -43,20 +50,24 @@ export function Address() {
 
           {step === 2 && <SelectedAddress />}
 
-          <div className="w-full flex justify-end">
-            <button
-              className="h-10 w-40 bg-[#62554a] hover:bg-[#998676] text-sm text-white uppercase rounded-lg mt-5 transition-all duration-200 flex items-center justify-center"
-              onClick={() => {
-                if (!state.selectedAddress) {
-                  alert("Choose an address");
-                  return;
-                }
-                setStep((prev) => prev + 1);
-              }}
-            >
-              {step === 1 ? "Proceed" : "Confirm"}
-            </button>
-          </div>
+          {step === 3 && <Payments />}
+
+          {step < 3 && (
+            <div className="w-full flex justify-end">
+              <button
+                className="h-10 w-40 bg-[#62554a] hover:bg-[#998676] text-sm text-white uppercase rounded-lg mt-5 transition-all duration-200 flex items-center justify-center"
+                onClick={() => {
+                  if (!state.selectedAddress) {
+                    alert("Choose an address");
+                    return;
+                  }
+                  setStep((prev) => prev + 1);
+                }}
+              >
+                {step === 1 ? "Proceed" : "Confirm"}
+              </button>
+            </div>
+          )}
         </div>
       ) : (
         <div>
