f69522896ea8325606f3559a908b90d13fa04406
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-26 22:17:43 +0100

    reduced spacings on smaller screens

--- app/(client)/checkout/page.tsx
+++ app/(client)/checkout/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="w-[95%] lg:w-[90%] mx-auto py-10 xs:py-16 space-y-16">
+    <div className="w-[95%] lg:w-[90%] mx-auto py-10 xs:py-16 space-y-8 sm:space-y-16">
       <h1 className="text-[30px] sm:text-4xl lg:text-5xl font-medium text-[#62554a]">
         Checkout
       </h1>
