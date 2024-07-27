fe85b102b77278650cdca64faab9d1a7a13d26fa
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-27 12:09:58 +0100

    did item exist check on action creator and remived in reducer

--- context/reducer.ts
+++ context/reducer.ts
@@ -3,14 +3,6 @@ import { ActionType, StateType } from "@/typing";
 export const reducer = (state: StateType, action: ActionType) => {
   switch (action.type) {
     case "ADD_ITEM_TO_CART":
-      const itemAlreadyExist = state.cart.find(
-        (item) => item.id === action.payload.id
-      );
-
-      if (itemAlreadyExist) {
-        return state;
-      }
-
       return { ...state, cart: [action.payload, ...state.cart] };
 
     case "CLEAR_CART":
