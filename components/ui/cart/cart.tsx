7498f421f5849b06c86a5e194b806deee486e991
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-27 08:12:37 +0100

    codntiong for rendering empty cart component added

--- components/ui/cart/cart.tsx
+++ components/ui/cart/cart.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import CartItems from "./cart-items";
 import CartReceipt from "./cart-receipt";
 import { CartPageSkeleton } from "@/components/skeletons";
+import EmptyCart from "./empty-cart";
 
 export default function Cart() {
   const { state, getTotal, increaseItem, decreaseItem, deleteItem } = useCart();
@@ -19,6 +20,12 @@ export default function Cart() {
   if (!isClient) {
     // Render a fallback or loading state during hydration
     return <CartPageSkeleton />;
+  }
+
+  if (!state.cart.length) {
+    return (
+      <EmptyCart text="Your Cart Is Empty" />
+    );
   }
 
   return (
