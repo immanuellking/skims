68d37d9181895b8cb4819aad204c445de6b7d0fc
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-30 23:59:52 +0100

    store page, moved code from static route to here

---
+++ components/ui/store/store-page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Store from "./store";
+
+export default function StorePage() {
+  return (
+    <main>
+      <div className="relative w-full h-[45vh] sm:h-[40vh] lg:h-[60vh]">
+        <Image
+          src="/hero/hero-2.webp"
+          alt="hero-img"
+          fill
+          className="object-cover object-center"
+        />
+        <div className="absolute left-2 xs:left-4 bottom-4 sm:bottom-0 sm:left-10 sm:inset-0 flex flex-col justify-center">
+          <h1 className="text-3xl sm:text-6xl text-white font-semibold uppercase">
+            new arrivals
+          </h1>
+          <p className="text-sm sm:text-base text-white font-medium">
+            shop the latest collections and must-have styles from SKIMS
+          </p>
+        </div>
+      </div>
+
+      <Store />
+    </main>
+  );
+}
