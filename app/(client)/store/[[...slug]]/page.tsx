7138ffaaa36643f3d98232d9fc021e9a69af730f
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-30 23:58:44 +0100

    moved code to its own components and rendered the store page or product details page based on the params

--- app/(client)/store/[[...slug]]/page.tsx
+++ app/(client)/store/[[...slug]]/page.tsx
@@ -1,24 +1,18 @@
-import ProductDetails from "@/components/ui/products/product-details";
-import Products from "@/components/ui/products/products";
-import { getProduct } from "@/lib/queries";
+import { ProductDetailsPageSkeleton } from "@/components/skeletons";
+import ProductDetailsPage from "@/components/ui/products/product-details-page";
+import StorePage from "@/components/ui/store/store-page";
+import { Suspense } from "react";
 
 export default async function Page({ params }: { params: { slug: string[] } }) {
-  const [type, slug] = params.slug;
-  const item = await getProduct(type, slug);
-  return (
-    <main className="w-[90%] mx-auto pt-8 xs:pt-10 lg:pt-16 pb-16 xs:pb-20 lg:pb-24">
-      <ProductDetails item={item} />
+  if (!params?.slug || params.slug.length === 0) {
+    return <StorePage />;
+  }
 
-      <div className="mt-16 xs:mt-20 lg:mt-28">
-        <div className="w-full text-center text-[#62554A] text-lg xs:text-xl sm:text-2xl lg:text-3xl font-medium mb-10 xs:mb-14">
-          <h1>MORE ITEMS WE THINK YOU MIGHT LIKE</h1>
-        </div>
-        <Products
-          type={type}
-          parentClass="w-[95%] sm:w-[90%] mx-auto"
-          cardClass="basis-1/2 sm:basis-1/3 lg:basis-1/4 overflow-hidden"
-        />
-      </div>
-    </main>
+  const [type, slug] = params?.slug;
+
+  return (
+    <Suspense fallback={<ProductDetailsPageSkeleton />}>
+      <ProductDetailsPage type={type} slug={slug} />
+    </Suspense>
   );
 }
