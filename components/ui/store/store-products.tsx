2f495711f2196da52a1e29024fc1e0e711ff8dc1
immanuellking <emmaebubeegbosi@gmail.com>
2024-07-31 00:04:35 +0100

    store product section done with sheet and sort component added and done

---
+++ components/ui/store/store-products.tsx
@@ -0,0 +1,50 @@
+import { fetchProducts } from "@/lib/queries";
+import { Sort } from "./sort";
+import Product from "./product";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "../button";
+import Filter from "./filter";
+
+export default async function StoreProducts() {
+  const { products, error } = await fetchProducts();
+  //   console.log("Products", products);
+  return (
+    <section className="w-full lg:px-8">
+      <div className="w-full flex flex-row-reverse justify-between lg:flex-row lg:justify-end">
+        <div className="lg:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                className="bg-[#AB8F80] hover:bg-[#AB8F80]/80 text-white font-medium hover:text-white"
+              >
+                Open
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Filter</SheetTitle>
+                <SheetDescription>
+                  <Filter />
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+        <Sort />
+      </div>
+      <div className="grid grid-cols-12 gap-y-10 gap-x-2 xs:gap-x-4 sm:gap-x-8 lg:gap-16 mt-10">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+}
