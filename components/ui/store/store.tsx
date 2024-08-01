574b2cb63258e491a0868a156b3cc7474d83eaa6
immanuellking <emmaebubeegbosi@gmail.com>
2024-08-01 14:57:57 +0100

    refactored and removed storeProducts component and destructured children

--- components/ui/store/store.tsx
+++ components/ui/store/store.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Filter from "./filter";
-import StoreProducts from "./store-products";
 
-export default function Store() {
+export default function Store({children} : {children: ReactNode}) {
   return (
     <section className="w-[95%] sm:w-[90%] lg:w-[85%] mx-auto py-10 sm:py-16 flex flex-col lg:flex-row">
       <div className="hidden lg:basis-[25%] lg:block ">
         <Filter />
       </div>
-      <StoreProducts />
+      {children}
     </section>
   );
 }
