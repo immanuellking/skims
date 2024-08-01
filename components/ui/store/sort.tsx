935feba9fe3b85d6c09ff5eb11579b71740db8ad
immanuellking <emmaebubeegbosi@gmail.com>
2024-08-01 14:49:12 +0100

    sort component with functionality of checking price in a order

--- components/ui/store/sort.tsx
+++ components/ui/store/sort.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Select,
   SelectContent,
@@ -7,10 +8,25 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export function Sort() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleChange = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("price", value);
+      router.replace(`${pathname}?${params.toString()}`);
+    } else {
+      params.delete("price");
+    }
+  };
+
   return (
-    <Select>
+    <Select onValueChange={handleChange}>
       <SelectTrigger className="w-[100px] bg-[#AB8F80] hover:bg-[#AB8F80]/80 focus:outline-none focus:border-none focus:ring-0 text-white font-medium">
         <SelectValue placeholder="Sort" />
       </SelectTrigger>
@@ -18,7 +34,7 @@ export function Sort() {
         <SelectGroup>
           <SelectLabel>Sort</SelectLabel>
           <SelectItem value="low-to-high">Price, Low to High</SelectItem>
-          <SelectItem value="High-to-Low">Price, High to Low</SelectItem>
+          <SelectItem value="high-to-Low">Price, High to Low</SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
