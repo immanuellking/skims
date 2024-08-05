f760e490f78afc7801c9124eaabba32895af8bbb
immanuellking <emmaebubeegbosi@gmail.com>
2024-08-05 14:22:10 +0100

    added route functionality to the button

--- components/shop-now.tsx
+++ components/shop-now.tsx
@@ -1,14 +1,20 @@
+"use client";
 import { shopNow } from "@/lib/data";
-import Button from "./button";
+import { useRouter } from "next/navigation";
 
 export default function ShopNow() {
+  const router = useRouter();
   return (
     <section className="w-full flex-col lg:flex-row flex gap-6">
       {shopNow.map((cat, idx) => (
         <div
           key={idx}
           className="relative h-[55vh] xs:h-[45vh] sm:h-[70vh] lg:h-[85vh] w-full"
-          style={{ backgroundImage: `url(${cat.img})`, backgroundPosition: "center", backgroundSize: "cover" }}
+          style={{
+            backgroundImage: `url(${cat.img})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
         >
           <div className="absolute bottom-[1.5rem] xs:bottom-[2rem] sm:bottom-[3rem] left-[1rem] sm:left-[2rem] space-y-2 xs:space-y-4">
             <div className="w-64 xs:w-72 sm:w-96 lg:w-full sm:space-y-2">
@@ -17,7 +23,12 @@ export default function ShopNow() {
               </h1>
               <p className="text-sm text-white">{cat.text}</p>
             </div>
-            <Button />
+            <button
+              onClick={() => router.push("/store")}
+              className="w-56 xs:w-64 bg-[#EEE4DA] py-2 xs:py-3 rounded-md font-semibold transition-all duration-200 ease-in-out hover:text-white text-sm xs:text-base hover:bg-[#d9bb9e]"
+            >
+              Shop Now
+            </button>
           </div>
         </div>
       ))}
